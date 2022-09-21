30825ebdf923af814b3d1468bdfabd1ca8d39832
siddharthpropelius <siddharth.vaishnav@propelius.tech>
2022-09-21 12:40:57 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReducer, useState } from 'react';
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case 'add':
+      return [...todos, newTodo(action.payload.name)];
+    case 'complete':
+      return todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete };
+        }
+        return todo;
+      });
+    case 'delete':
+      return todos.filter((todo) => todo.id !== action.payload.id);
+
+    default:
+      return todos;
+  }
+}
+
+function newTodo(name) {
+  return { id: Math.random(), name: name, complete: false };
+}
 
 function App() {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setName('');
+    dispatch({ type: 'add', payload: { name: name } });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      UseReducer
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="enter a name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          required
+        />
+      </form>
+      <div>
+        {todos.map((todo) => {
+          return (
+            <div
+              style={{ display: 'flex', justifyContent: 'center' }}
+              key={todo.id}
+            >
+              <p
+                style={{ color: todo.complete ? '#AAA' : 'black' }}
+                id={todo.id}
+              >
+                {todo.name}
+              </p>
+              <p>
+                <button
+                  onClick={() =>
+                    dispatch({
+                      type: 'complete',
+                      payload: { id: todo.id },
+                    })
+                  }
+                >
+                  complete
+                </button>
+                <button
+                  onClick={() =>
+                    dispatch({ type: 'delete', payload: { id: todo.id } })
+                  }
+                >
+                  delete
+                </button>
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
